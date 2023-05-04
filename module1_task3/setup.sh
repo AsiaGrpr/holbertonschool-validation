33187f9632d47f08e6fb232b24ea974951c2e0dd
AsiaGrpr <groupierre.asia@gmail.com>
2023-05-04 07:38:03 -0700

    write a setup file

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,8 +1,5 @@
-#!/bin/bash -e
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-mkdir hugo
-tar -xzf ./hugo_extended_0.84.0_Linux-64bit.tar.gz -C ./hugo
-unlink hugo_extended_0.84.0_Linux-64bit.tar.gz
-export PATH=$PATH:./hugo
-chmod u+x ./hugo/hugo
-echo "Successfully initialized"
+#!/bin/bash
+
+apt-get update
+apt-get install -y hugo make
+make build
